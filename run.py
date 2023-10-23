2f46f303787cd75bf5c6e62403d7ed864b35c252
aadit-chavan <148774243+aadit-chavan@users.noreply.github.com>
2023-10-23 20:34:42 +0530

    Update run.py

--- run.py
+++ run.py
@@ -3,12 +3,14 @@ import torch
 
 device = "cuda"
 
-model = AutoModelForCausalLM.from_pretrained("model_name", torch_dtype=torch.float16)
-tokenizer = AutoTokenizer.from_pretrained("model_name")
+model = AutoModelForCausalLM.from_pretrained("Mistral-7B-v0.1", torch_dtype=torch.float16)
+tokenizer = AutoTokenizer.from_pretrained("Mistral-7B-v0.1")
+
+model = model.to("cuda")
 
 while True:
     input_text = input("You: ")
-    input_ids = tokenizer.encode(input_text, return_tensors="pt")
+    input_ids = tokenizer.encode(input_text, return_tensors="pt").to("cuda")
 
     output = model.generate(input_ids)
 
@@ -16,4 +18,3 @@ while True:
 
     print(f"Model: {output_text}")
 
-    
